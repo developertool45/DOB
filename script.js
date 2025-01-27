aa58e5bf989cb28f3a67b72b8492b26a4a6a79ae
developertool45 <developertool45@gmail.com>
2025-01-27 16:23:07 +0530

    udpate

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 let dateInput = document.querySelector("#date");
-dateInput.addEventListener("change", function (event) {
-  let userDate = new Date(event.target.value);
-  let userYear = userDate.getFullYear();
-  let userMonth = userDate.getMonth() + 1;
-  let userDay = userDate.getDate();
-  calculateAge(userYear, userMonth, userDay);
+let today = new Date();
+let curYear, curMonth, curDay;
+let formData = document.querySelector(".form");
+
+formData.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  let year = document.querySelector("#year").value;
+  let month = document.querySelector("#month").value;
+  let day = document.querySelector("#day").value;
+  validateDate(year, month, day);
 });
 
+function validateDate(year, month, day) {
+  if (year.length !== 4 || year < 1900 || year > today.getFullYear()) {
+    alert(" year should be 1900 to " + today.getFullYear());
+    return;
+  }
+  if (month < 1 || month > 12) {
+    alert("Month should be between 01 and 12");
+    return;
+  }
+  if (day < 1 || day > 31) {
+    alert("Day should be between 01 and 31");
+    return;
+  }
+  calculateAge(year, month, day);
+}
 function calculateAge(year, month, day) {
-  let today = new Date();
-  let curYear = today.getFullYear();
-  let curMonth = today.getMonth() + 1;
-  let curDay = today.getDate();
-
+  curYear = today.getFullYear();
+  curMonth = today.getMonth() + 1;
+  curDay = today.getDate();
   if (curDay < day) {
     curDay += 30;
     curMonth--;
@@ -22,11 +39,10 @@ function calculateAge(year, month, day) {
     curMonth += 12;
     curYear--;
   }
-
-  displayAge(curYear, curMonth, curDay, year, month, day);
+  displayAge(year, month, day);
 }
 
-function displayAge(curYear, curMonth, curDay, year, month, day) {
+function displayAge(year, month, day) {
   let ageYears = curYear - year;
   let ageMonths = curMonth - month;
   let ageDays = curDay - day;
@@ -41,7 +57,8 @@ function attempts(ageYears, ageMonths, ageDays) {
   let totalYears = ageYears + ageMonths / 12 + ageDays / 365;
   let result = document.querySelector(".result h3");
   let showAttempts = document.querySelector(".result p");
-  let attempts = 0;
+
+  let attempts;
   if (totalYears < 16.5) {
     let remainYears = 16.5 - ageYears;
     result.textContent = `please wait for ${Math.abs(remainYears).toFixed(
